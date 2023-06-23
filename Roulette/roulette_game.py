77f1c3e2169fa931c2a2e5d4070b08cdfdd5dab1
Sterling <devprofile.spfeiffer@gmail.com>
2023-06-23 02:14:49 -0700

    commenting

--- Roulette/roulette_game.py
+++ Roulette/roulette_game.py
@@ -1,23 +1,24 @@
+#import random module and define range 1 to 6 inclusive
 import random 
 key_num = random.randint (1,6)
 
-print (key_num)
+#for testing/debugging only
+##print (key_num)
 
 guess = int(input ("Guess a number between 1 and 6 "))
 
+#function requires str() for sorting algorithm
 def algorithm_direction(key_direction):
      global guess
      print ("Too " + key_direction)
      guess = int (input ("Guess Again. "))
 
+#sorting algorithm
 while guess != key_num:
-     
      if guess > key_num:
           algorithm_direction ("high")
-
      if guess < key_num:
           algorithm_direction ("low")
-
 if guess == key_num:
      print ("Correct")
 
